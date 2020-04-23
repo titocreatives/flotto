edc7b0f6e6379b58bea05e1c915d7d2e9f967110
Lucas Tito Kotaranin <lucas.kotaranin@dreamit.de>
2020-04-23 16:57:48 +0200

    prettier fix

--- src/components/organisms/hero-numbers/hero-numbers.vue
+++ src/components/organisms/hero-numbers/hero-numbers.vue
@@ -6,7 +6,10 @@
         v-for="(number, i) in [...numbers, ...additionalNumbers]"
         :key="`${number}-${i}-${Math.random()}`"
       >
-        <figure class="ball" :class="i < numbers.length ? 'as-default' : 'as-additional'">
+        <figure
+          class="ball"
+          :class="i < numbers.length ? 'as-default' : 'as-additional'"
+        >
           <span class="ball-number f-number">{{ number }}</span>
           <span class="ball-shadow" />
         </figure>
